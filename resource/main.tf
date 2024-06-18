f98da1a652b9c5d03277cb65726054ebf12e0ca6
kp3073 <keyurpatel8288@gmail.com>
2024-06-18 15:29:01 -0400

    1

--- resource/main.tf
+++ resource/main.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "web" {
-  ami           = ami-0b4f379183e5706b9
+resource "aws_instance" "ec2" {
+  ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
 
   tags = {
