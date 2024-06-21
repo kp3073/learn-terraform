28cc440a328e44c84410f5ace3caa37d5896e77e
kp3073 <keyurpatel8288@gmail.com>
2024-06-21 12:44:53 -0400

    1

--- resource/main.tf
+++ resource/main.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "this" {
   ami = "ami-0b4f379183e5706b9"
-
   instance_type = "t3.micro"
+  security_groups = "sg-0411ee85e37f2519a"
+
   tags = {
     Name = "test-spot"
   }
