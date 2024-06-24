c7c215b332e14923ed26dc90d79ff375bde68738
kp3073 <keyurpatel8288@gmail.com>
2024-06-24 16:24:25 -0400

    1

---
+++ s3/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-d75"
+    key    = "/terraform-series/terrraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+resource "null_resource" "name" {
+  resource "local-exec"{
+    command = "echo Hello World"
+  }
+}
