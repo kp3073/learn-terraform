800f8de6cbf0dd836e8217e9d204aabe2b2b9b4d
kp3073 <keyurpatel8288@gmail.com>
2024-07-03 23:53:15 -0400

    1

---
+++ modules/input.tf
@@ -0,0 +1,3 @@
+vpc_cidr = [""10.0.0.0/16""]
+account_no = "471112727668"
+default_vpc_id = "vpc-01c37a20026cef1d0"
--- modules/vars.tf
+++ modules/vars.tf
@@ -1,11 +1,3 @@
-variable "vpc_cidr" {
-  default = [""10.0.0.0/16""]
-}
-
-variable "account_no" {
-  default = "471112727668"
-}
-
-variable "default_vpc_id" {
-  default = "vpc-01c37a20026cef1d0"
-}
+variable "vpc_cidr" {}
+variable "account_no" {}
+variable "default_vpc_id" {}
