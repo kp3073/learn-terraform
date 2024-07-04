6dc79482a2ac7b6bff0a9997853fac97a3b434fa
kp3073 <keyurpatel8288@gmail.com>
2024-07-04 00:08:32 -0400

    1

--- test/main.tf
+++ test/main.tf
@@ -8,9 +8,10 @@ resource "aws_vpc" "main" {
 
 
 resource "aws_vpc_peering_connection" "foo" {
-  peer_owner_id = "${var.peer_owner_id}"
-  peer_vpc_id   = "${aws_vpc.main.id}"
+  peer_owner_id = var.peer_owner_id
+  peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.vpc_id
+  auto_accept = true
 }
 
 
