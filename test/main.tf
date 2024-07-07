ec6f6c706b715258d33118927a683eb0c24254ef
kp3073 <keyurpatel8288@gmail.com>
2024-07-06 21:34:58 -0400

    1

--- test/main.tf
+++ test/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc_peering_connection" "foo" {
 resource "aws_subnet" "main" {
   count = length(var.privet_subent)
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.privet_subent.[count.index]
+  cidr_block = var.privet_subent[count.index]
 
   tags = {
     Name = "privet_subent"
