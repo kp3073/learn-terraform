ed46acfb4851367778ac952be9acd2dd7a43499e
kp3073 <keyurpatel8288@gmail.com>
2024-07-19 23:54:27 -0400

    11

--- series/project/main.tf
+++ series/project/main.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
 
 resource "aws_s3_bucket" "bucket" {
   bucket = "my-tf-test-bucket"
+  region = "us-east-1"
 }
