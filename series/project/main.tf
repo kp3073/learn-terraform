3a425fbd93f6b2a6381bfaf5a9db9e67b8dd8a7a
kp3073 <keyurpatel8288@gmail.com>
2024-07-20 00:01:31 -0400

    11

--- series/project/main.tf
+++ series/project/main.tf
@@ -3,10 +3,10 @@ resource "aws_s3_bucket" "bucket12" {
   bucket = "my-tf-test-bucket9999asd9u942"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "my-tf-test-bucket9999asd9u942"
-#     key    = "test"
-#     region = "us-east-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "my-tf-test-bucket9999asd9u942"
+    key    = "test"
+    region = "us-east-1"
+  }
+}
