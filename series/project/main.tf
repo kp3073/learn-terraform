744c5531ed7bc4b2015e3e70b5bf1158c1f96971
kp3073 <keyurpatel8288@gmail.com>
2024-07-20 00:10:32 -0400

    11

--- series/project/main.tf
+++ series/project/main.tf
@@ -1,10 +1,4 @@
 
-
-resource "aws_s3_bucket" "bucket12" {
-  bucket = "my-tf-test-bucket9999asd9u942"
-}
-
-
 backend "s3" {
   bucket         	   = "my-tf-test-bucket9999asd9u942"
   key              	   = "terraform.tfstate"
