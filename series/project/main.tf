62120451a856aab4c0eada7ccb0f56f581f196fd
kp3073 <keyurpatel8288@gmail.com>
2024-07-20 11:32:03 -0400

    11

--- series/project/main.tf
+++ series/project/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.6.2"
-    }
-  }
-}
-
-resource "random_id" "bucket_name" {
-  byte_length = 8
-}
-
-resource "aws_s3_bucket" "buc" {
-  bucket = "bucket-${random_id.bucket_name.hex}"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "mytestdevops"
 }
