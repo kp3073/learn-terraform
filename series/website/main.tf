5153b30fd62c1ae5673ce944ac9a8676ccec1579
kp3073 <keyurpatel8288@gmail.com>
2024-07-20 23:50:45 -0400

    11

--- series/website/main.tf
+++ series/website/main.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = "mytestdevops819274987249732974937"
 }
+
+resource "aws_s3_object" "file" {
+  bucket = aws_s3_bucket.bucket.bucket
+  source = "./terraform.tfstate"
+  key    = "terraform.tfstate"
+}
