ecff55b7ea60e45260a72352023fd26db32fa5eb
kp3073 <keyurpatel8288@gmail.com>
2024-07-21 00:12:19 -0400

    11

--- series/website/main.tf
+++ series/website/main.tf
@@ -5,11 +5,11 @@ terraform {
       version = "3.6.2"
     }
   }
-#   backend "s3" {
-#     bucket = aws_s3_bucket.bucket.bucket
-#     key = "backend.tfstate"
-#     region = "us-east-1"
-#   }
+  backend "s3" {
+    bucket = "mytestdevops-8c5cd2f3193c3d2f"
+    key = "backend.tfstate"
+    region = "us-east-1"
+  }
 }
 
 resource "random_id" "buket_id" {
