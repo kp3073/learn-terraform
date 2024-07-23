39fc53bb9644754cf807c417c4a2282647c9c2cf
kp3073 <keyurpatel8288@gmail.com>
2024-07-23 15:53:56 -0400

    11

--- series/static-website/main.tf
+++ series/static-website/main.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-DevOps-kp3073111"
+resource "aws_s3_bucket" "website" {
+  bucket = "aws_s3_bucket.kp30731991"
 }
 #
 resource "aws_s3_object" "html" {
-  bucket = aws_s3_bucket.example.bucket
+  bucket = aws_s3_bucket.website.bucket
   key    = "index.html"
   source = "./index.html"
 }
 
 resource "aws_s3_object" "styles" {
-  bucket = aws_s3_bucket.example.bucket
+  bucket = aws_s3_bucket.website.bucket
   key    = "styles.css"
   source = "./styles.css"
 }
