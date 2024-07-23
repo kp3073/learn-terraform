be957abad1c3c1ae069ab84819743e18454b437c
kp3073 <keyurpatel8288@gmail.com>
2024-07-23 14:23:58 -0400

    11

--- series/static-website/main.tf
+++ series/static-website/main.tf
@@ -4,6 +4,6 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_s3_object" "html" {
   bucket = "my-tf-test-bucket-DevOps"
-  key    = "new_object_key"
-  source = "path/to/file"
+  key    = "index.html"
+  source = "./index.html"
 }
