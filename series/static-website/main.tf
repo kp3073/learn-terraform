9b010b72998776b859c63589141826b204a8e06b
kp3073 <keyurpatel8288@gmail.com>
2024-07-23 15:12:32 -0400

    11

--- series/static-website/main.tf
+++ series/static-website/main.tf
@@ -3,8 +3,14 @@ resource "aws_s3_bucket" "example" {
   region = "us-east-1"
 }
 #
-# resource "aws_s3_object" "html" {
-#   bucket = "my-tf-test-bucket-DevOps-kp3073"
-#   key    = "index.html"
-#   source = "./index.html"
-# }
+resource "aws_s3_object" "html" {
+  bucket = aws_s3_bucket.example.bucket
+  key    = "index.html"
+  source = "./index.html"
+}
+
+resource "aws_s3_object" "styles" {
+  bucket = aws_s3_bucket.example.bucket
+  key    = "styles.css"
+  source = "./styles.css"
+}
