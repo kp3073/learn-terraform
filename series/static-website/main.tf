a1e5270f5768a98ece322f602c4c89b542ba74b2
kp3073 <keyurpatel8288@gmail.com>
2024-07-24 18:56:46 -0400

    11

--- series/static-website/main.tf
+++ series/static-website/main.tf
@@ -11,16 +11,48 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = false
 }
 
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Sid = "PublicReadGetObject",
+          Effect = "Allow",
+          Principal = "*",
+          Action = "s3:GetObject",
+          Resource = "arn:aws:s3:::${aws_s3_bucket.website.id}/*"
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
 
 
 resource "aws_s3_object" "html" {
   bucket = aws_s3_bucket.website.bucket
   key    = "index.html"
   source = "./index.html"
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "styles" {
   bucket = aws_s3_bucket.website.bucket
   key    = "styles.css"
   source = "./styles.css"
+  content_type = "text/css"
+}
+
+output "wesiteurl" {
+  value = aws_s3_bucket_website_configuration.example.website_endpoint
 }
