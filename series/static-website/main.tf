faa69e8f4c0d3a26417a2a4455e46a3dc2c9b860
kp3073 <keyurpatel8288@gmail.com>
2024-07-23 23:33:02 -0400

    11

--- series/static-website/main.tf
+++ series/static-website/main.tf
@@ -1,7 +1,18 @@
 resource "aws_s3_bucket" "website" {
   bucket = "aws-s3-bucketkp307319911"
 }
-#
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+
 resource "aws_s3_object" "html" {
   bucket = aws_s3_bucket.website.bucket
   key    = "index.html"
