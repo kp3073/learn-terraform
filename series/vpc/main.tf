d2c3c9fa83d14bbaa180e2b8d448057149d66e33
kp3073 <keyurpatel8288@gmail.com>
2024-07-29 00:10:48 -0400

    111

--- series/vpc/main.tf
+++ series/vpc/main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "my-vpc" {
 
 resource "aws_subnet" "private-subnet" {
   vpc_id = aws_vpc.my-vpc.id
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.1.0/24"
   tags = {
     Name = "Private-Subnet"
   }
@@ -15,7 +15,7 @@ resource "aws_subnet" "private-subnet" {
 
 resource "aws_subnet" "public-subnet" {
   vpc_id = aws_vpc.my-vpc.id
-  cidr_block = "10.0.2.0/16"
+  cidr_block = "10.0.2.0/24"
   tags = {
     Name = "Public-Subnet"
   }
