533cab03dd1c57ee65c66fd53f24fb03af27ae2c
kp3073 <keyurpatel8288@gmail.com>
2025-03-25 15:30:26 -0400

    kp | docker

--- series/foreach/variables.tf
+++ series/foreach/variables.tf
@@ -2,12 +2,14 @@ variable "env" {}
 
 
 variable "vms" {
-  vm-test1 = {
-	name    = "mysql"
-	vm_size = "Standard_DS1_v2"
-  }
-  vm-test2 = {
-	name    = "mongodb"
-	vm_size = "Standard_DS1_v2"
+  default = {
+	vm-test1 = {
+	  name    = "mysql"
+	  vm_size = "Standard_DS1_v2"
+	}
+	vm-test2 = {
+	  name    = "mongodb"
+	  vm_size = "Standard_DS1_v2"
+	}
   }
 }
