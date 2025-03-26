55fe4880f829302f3b0b525d50c6a0d26795d7e0
kp3073 <keyurpatel8288@gmail.com>
2025-03-26 12:59:58 -0400

    kp | docker

--- series/foreach/main.tf
+++ series/foreach/main.tf
@@ -96,7 +96,7 @@ resource "azurerm_virtual_machine" "main" {
 
 resource "azurerm_dns_a_record" "private" {
   for_each            = var.vms
-  name                = azurerm_virtual_machine.main[each.key].name
+  name                = "${ azurerm_virtual_machine.main[each.key].name }-internal"
   zone_name           = "cloudaws.online"
   resource_group_name = data.azurerm_resource_group.rg.name
   ttl                 = 30
