de11fd4b5e6b61debee2615916a1c80ac1afb3e9
kp3073 <keyurpatel8288@gmail.com>
2025-03-26 12:44:58 -0400

    kp | docker

--- series/foreach/main.tf
+++ series/foreach/main.tf
@@ -10,20 +10,6 @@ resource "azurerm_public_ip" "main" {
   }
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = data.azurerm_virtual_network.vnet.name
-  address_space = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "main" {
-  name                 = "default"
-  resource_group_name  = data.azurerm_resource_group.rg.location
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes = ["10.0.2.0/24"]
-}
-
 
 resource "azurerm_network_interface" "main" {
   for_each            = var.vms
