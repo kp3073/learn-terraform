806d94f7f78a1a83e488eda76818e328aae448e5
kp3073 <keyurpatel8288@gmail.com>
2025-03-26 11:14:38 -0400

    kp | docker

--- series/foreach/main.tf
+++ series/foreach/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "main" {
 resource "azurerm_subnet" "internal" {
   name                 = "default"
   resource_group_name  = data.azurerm_resource_group.rg.location
-  virtual_network_name = data.azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes = ["10.0.0.0/24"]
 }
 
